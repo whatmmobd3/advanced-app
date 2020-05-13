1e94d60918d87f8c2655067b9ce2c48b0b9b01d0
quangloc.nguyen <quangloc.nguyen>
2020-05-13 11:35:58 +0700

    verify

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import CommentBox from './CommentBox';
+import CommentList from './CommentList';
 
 export default () => {
     return(
         <div>
-            App Component
+           <CommentBox />
+           <CommentList />
         </div>
     )
 }

3b04d952a4cb27093baa9c779243f071862e9e99
quangloc.nguyen <quangloc.nguyen>
2020-05-14 11:31:43 +0700

    expectation components instances

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../components/App'
+import React from "react";
+import {shallow} from 'enzyme';
+import App from "../components/App";
+import CommentBox from "../components/CommentBox";
+import CommentList from "../components/CommentList";
 
-it('shows a comment box', () => {
-    const div = document.createElement('div')
-
-    ReactDOM.render(<App />, div)
-    // console.log(div.innerHTML)
-    expect(div.innerHTML).toContain('Comment List')
-
-    ReactDOM.unmountComponentAtNode(div)
-})
+it("shows a comment box", () => {
+  const wrapped = shallow(<App />);
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+});

b56fb0152012aa0a33c3f8eb760e86a662d58a19
quangloc.nguyen <quangloc.nguyen>
2020-05-18 09:51:20 +0700

    Asserting

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,15 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import {mount} from 'enzyme'
+import { mount } from "enzyme";
 import CommentBox from "../CommentBox";
 
-it('has a text area and a button', () => {
-
-    const wrapped = mount(<CommentBox />)
-    console.log(wrapped.find('textarea'));
-    console.log(wrapped.find('button'));
-    
-    
+it("has a text area and a button", () => {
+  const wrapped = mount(<CommentBox />);
 
 
-})
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
